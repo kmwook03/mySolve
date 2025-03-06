dd72834166908fe84b7a1b168e865090b8b07a9f
kmwook03 <114015128+kmwook03@users.noreply.github.com>
2025-03-06 21:47:36 +0900

    [level 0] Title: 약수 구하기, Time: 0.06 ms, Memory: 4.21 MB -BaekjoonHub

---
+++ 프로그래머스/0/120897. 약수 구하기/약수 구하기.cpp
@@ -0,0 +1,15 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+vector<int> solution(int n) {
+    vector<int> answer = {1};
+    
+    for (int i=2; i<=n; i++)
+    {
+        if (n%i==0) answer.push_back(i);
+    }
+    
+    return answer;
+}
