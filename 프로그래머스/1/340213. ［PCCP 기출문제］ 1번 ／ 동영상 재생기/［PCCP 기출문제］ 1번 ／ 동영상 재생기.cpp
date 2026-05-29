7758a0c2ae23cac9354f8561e18a1e3d23ded193
MinWook <114015128+kmwook03@users.noreply.github.com>
2026-05-30 01:49:18 +0900

    [level 1] Title: [PCCP 기출문제] 1번 / 동영상 재생기, Time: 0.03 ms, Memory: 4.07 MB -BaekjoonHub

--- 프로그래머스/1/340213. ［PCCP 기출문제］ 1번 ／ 동영상 재생기/［PCCP 기출문제］ 1번 ／ 동영상 재생기.cpp
+++ 프로그래머스/1/340213. ［PCCP 기출문제］ 1번 ／ 동영상 재생기/［PCCP 기출문제］ 1번 ／ 동영상 재생기.cpp
@@ -1,38 +1,61 @@
 #include <string>
 #include <vector>
+#include <sstream>
 
 using namespace std;
 
+int time_to_sec(string t) {
+    stringstream ss(t);
+    string token;
+    vector<int> time;
+    
+    while (getline(ss, token, ':'))
+        time.push_back(stoi(token));
+    
+    return time[0]*60 + time[1];
+}
+
+string sec_to_mmss(int time) {
+    string mm, ss;
+    int m = time / 60;
+    int s = time - m*60;
+    mm = (m < 10) ? "0" + to_string(m) : to_string(m);
+    ss = (s < 10) ? "0" + to_string(s): to_string(s);
+    
+    return mm + ":" + ss;
+}
+
+void prev(int& pos) {
+    if (pos < 10) 
+        pos = 0;
+    else 
+        pos -= 10;
+}
+
+void next(int vid_len, int& pos) {
+    if (vid_len - pos < 10)
+        pos = vid_len;
+    else
+        pos += 10;
+}
+
 string solution(string video_len, string pos, string op_start, string op_end, vector<string> commands) {
-    int v_len = (video_len[0] - '0')*1000 + (video_len[1] - '0')*100 + 
-                (video_len[3] - '0')*10 + (video_len[4] - '0');
-    int op_s = (op_start[0] - '0')*1000 + (op_start[1] - '0')*100 + 
-                (op_start[3] - '0')*10 + (op_start[4] - '0');
-    int op_e = (op_end[0] - '0')*1000 + (op_end[1] - '0')*100 + 
-                (op_end[3] - '0')*10 + (op_end[4] - '0');
-    int p = (pos[0] - '0')*1000 + (pos[1] - '0')*100 + 
-            (pos[3] - '0')*10 + (pos[4] - '0');
-    for (const auto cmd : commands)
-    {
-        if (p>=op_s && p<=op_e) p=op_e;
-        if (cmd == "next")
-        {
-            int next_p = ((p+10)%100 >= 60) ? (p+50) : (p+10);
-            p = (next_p>=v_len) ? (v_len) : (next_p);
-        }
-        if (cmd == "prev")
-        {
-            int next_p = ((p-10)%100 >= 60) ? (p-50) : (p-10);
-            p = (next_p<=0) ? (0) : (next_p);
-        }
+    int vid_len_sec = time_to_sec(video_len);
+    int pos_sec = time_to_sec(pos);
+    int op_start_sec = time_to_sec(op_start);
+    int op_end_sec = time_to_sec(op_end);
+    
+    for (auto c : commands) {
+        if (op_start_sec <= pos_sec && pos_sec <= op_end_sec)
+            pos_sec = op_end_sec;
+        if (c == "prev")
+            prev(pos_sec);
+        else if (c == "next")
+            next(vid_len_sec, pos_sec);
+        if (op_start_sec <= pos_sec && pos_sec <= op_end_sec)
+            pos_sec = op_end_sec;
     }
-    if (p>=op_s && p<=op_e) p=op_e;
-    string sp = to_string(p);
-    if (sp.length() < 4)
-    {
-        int now_len = sp.length();
-        for (int i=0; i<4-now_len; i++) sp = "0" + sp;
-    }
-    sp = sp.substr(0,2) + ":" + sp.substr(2,3);
-    return sp;
+    
+    string answer = sec_to_mmss(pos_sec);
+    return answer;
 }
